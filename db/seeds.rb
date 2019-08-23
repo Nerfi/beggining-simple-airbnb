7ed538af2bb46a415cb46b2d0b63b8f38dd584dc
Juan Paredes <juanchuu.jfpc@gmail.com>
2019-08-23 21:17:00 +0200

    index show new and create routes are implement

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,34 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Flat.destroy_all
+puts  "creating flats"
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+Flat.create!(
+  name: 'Light & Spacious ',
+  address: '10 Clifton  1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 85,
+  number_of_guests: 3
+)
+Flat.create!(
+  name: 'Dark flat',
+  address: 'plaza camachos nº3',
+  description: 'A lovely summer feel ',
+  price_per_night: 55,
+  number_of_guests: 3
+)
+Flat.create!(
+  name: 'flat fat',
+  address: 'poeta vicente medina W9 1DT',
+  description: 'a good flat in a nice area in Murcia city',
+  price_per_night: 7,
+  number_of_guests: 3
+)
+
+
+puts "done"
